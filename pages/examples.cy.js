8f0e36b64d17adc0cd6127948e266211d51f9d23
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-27 19:04:12 -0500

    add skipped failing router test

--- pages/examples.cy.js
+++ pages/examples.cy.js
@@ -4,3 +4,11 @@ it('shows all examples', () => {
   cy.mount(<IndexPage />)
   cy.get('[role=list] li').should('have.length.above', 10)
 })
+
+it.skip('navigates', () => {
+  cy.mount(<IndexPage />)
+  cy.contains('[role=list] li', 'button')
+    // tries to navigate, router is null
+    // Cannot read properties of null (reading 'push')
+    .click()
+})
